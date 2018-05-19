96e1cf49b526c9b707060f226ddc1062c50d5e1f
Alasdair Haigh <alasdair.haigh@gmail.com>
2018-05-19 13:50:40 +0100

    Hello world

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text } from 'react-native';
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hola Nerditaaaa!</Text>
-      </View>
+      <Text>Hello world!</Text>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
