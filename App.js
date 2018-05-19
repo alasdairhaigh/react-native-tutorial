51d3f88aa47544c8e128c2a549fa449970af90a9
Alasdair Haigh <alasdair.haigh@gmail.com>
2018-05-19 14:06:04 +0100

    Props: LotsOfGreetings - Passing name properties

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
+import { AppRegistry, Image, Text, View } from 'react-native';
 
-export default class Bananas extends Component {
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
-    );
+      <View style={{alignItems: 'center'}}>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Greeting name='Sterling'/>
+        <Greeting name='Pam'/>
+        <Greeting name='Lana'/>
+      </View>
+    )
   }
 }
